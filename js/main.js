d2a4cf450c0a08e53173507459f8740bc8866977
Clochs <sam_vs_computer@gmail.com>
2024-08-20 15:34:15 -0500

    Built grid and add mouseover effect

--- js/main.js
+++ js/main.js
@@ -1,21 +1,90 @@
 let button = document.querySelector('#btn')
-let main = document.querySelector('#main')
+let container = document.querySelector('#container')
+let inputBtn = document.querySelector('#inputBtn')
+let userChoice
+
+
+inputBtn.addEventListener('click', () => {
+    userChoice = Number(prompt('Enter a value: '))
+    console.log(userChoice)
+    console.log(typeof (userChoice))
+})
+button.addEventListener('click', () => {
+    for (let i = 0; i < userChoice; i++) {
+        let row = document.createElement('div')
+        row.classList.add('row')
+
+        for (let j = 0; j < userChoice; j++) {
+            let squares = document.createElement('div')
+            squares.classList.add('squares')
+
+
+            squares.addEventListener('mouseover', () => {
+                squares.classList.add('black')
+                console.log("Hovered")
+            })
+            row.appendChild(squares)
+        }
+        container.appendChild(row)
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let button = document.querySelector('#btn')
+// let main = document.querySelector('#main')
 
 
 // button.addEventListener('click', () => {
-    for (let i = 0; i < 100 * 100; i++) {
-        let squares = document.createElement('div')
-        squares.classList.add('squares')
-        main.appendChild(squares)
-    
-    squares.addEventListener('mouseover', () => {
-        console.log('hovered')
-        squares.classList.add('black')
+//     for (let i = 0; i < 16; i++){
+//         let row = document.createElement('div')
+//         row.classList.add('row')
 
-    })
-}
+//         for (let j = 0; j < 16; j++){
+//             let squares = document.createElement('div')
+//             squares.classList.add('squares')
 
-// })
+//             squares.addEventListener('mouseover', () => {
+//                 console.log('hovered')
+//                 squares.classList.add('black')
+//             })
+//             row.appendChild(squares)
+//         }
+//         main.appendChild(row)
+//         }
+//     })
 
 
 
