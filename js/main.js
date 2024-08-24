e8b2f3d586a27b9de1ba09dd7fe79418c8c1b8a0
Clochs <sam_vs_computer@gmail.com>
2024-08-23 20:44:09 -0500

    deleted everything and started over

--- js/main.js
+++ js/main.js
@@ -1,90 +1,11 @@
-let button = document.querySelector('#btn')
-let container = document.querySelector('#container')
-let inputBtn = document.querySelector('#inputBtn')
-let userChoice
+let inputBtn= document.querySelector('#input-btn')
+let container = document.querySelector('#container')   
 
-
+// IF inputBtn is clicked
+//      prompt the user for the grid size
+// IF grid size is less than 0 or greater than 100
+//      prompt the user again
+// INIT a variable with the user input
 inputBtn.addEventListener('click', () => {
-    userChoice = Number(prompt('Enter a value: '))
-    console.log(userChoice)
-    console.log(typeof (userChoice))
+    document.createElement('div')
 })
-button.addEventListener('click', () => {
-    for (let i = 0; i < userChoice; i++) {
-        let row = document.createElement('div')
-        row.classList.add('row')
-
-        for (let j = 0; j < userChoice; j++) {
-            let squares = document.createElement('div')
-            squares.classList.add('squares')
-
-
-            squares.addEventListener('mouseover', () => {
-                squares.classList.add('black')
-                console.log("Hovered")
-            })
-            row.appendChild(squares)
-        }
-        container.appendChild(row)
-    }
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let button = document.querySelector('#btn')
-// let main = document.querySelector('#main')
-
-
-// button.addEventListener('click', () => {
-//     for (let i = 0; i < 16; i++){
-//         let row = document.createElement('div')
-//         row.classList.add('row')
-
-//         for (let j = 0; j < 16; j++){
-//             let squares = document.createElement('div')
-//             squares.classList.add('squares')
-
-//             squares.addEventListener('mouseover', () => {
-//                 console.log('hovered')
-//                 squares.classList.add('black')
-//             })
-//             row.appendChild(squares)
-//         }
-//         main.appendChild(row)
-//         }
-//     })
-
-
-
