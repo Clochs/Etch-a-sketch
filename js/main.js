f3e60f810f4f1550b8eac69d04a4ee47e9214ada
Clochs <sam_vs_computer@gmail.com>
2024-08-23 22:13:00 -0500

    rebuilt js inputBtn, css squares and container, and html. None are finished

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
-let inputBtn= document.querySelector('#input-btn')
-let container = document.querySelector('#container')   
-
+let inputBtn = document.querySelector('#input-btn')
+let buildBtn = document.querySelector('#build-btn')
+let container = document.querySelector('#container')
+let squares
 // IF inputBtn is clicked
 //      prompt the user for the grid size
 // IF grid size is less than 0 or greater than 100
 //      prompt the user again
 // INIT a variable with the user input
 inputBtn.addEventListener('click', () => {
-    document.createElement('div')
+    let userChoice = Number(prompt("Enter a number between 1 and 100: "))
+    while (userChoice < 1 || userChoice > 100) {
+        userChoice = Number(prompt("Try again: "))
+    }
 })
+
+
+
