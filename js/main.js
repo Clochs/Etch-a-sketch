cfe37d03b4558c7ab068d9f4e93f29a567fbf495
Clochs <sam_vs_computer@gmail.com>
2024-08-24 21:14:01 -0500

    built input button

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
 let inputBtn = document.querySelector('#input-btn')
-let buildBtn = document.querySelector('#build-btn')
+let buildBtn = document.querySelector('#build-btnk')
 let container = document.querySelector('#container')
-let squares
+let userChoice
+let containerSize = 300
+let squareSize = containerSize / userChoice 
+
 // IF inputBtn is clicked
 //      prompt the user for the grid size
 // IF grid size is less than 0 or greater than 100
 //      prompt the user again
 // INIT a variable with the user input
 inputBtn.addEventListener('click', () => {
-    let userChoice = Number(prompt("Enter a number between 1 and 100: "))
+    userChoice = Number(prompt("Enter a number between 1 and 100: "))
     while (userChoice < 1 || userChoice > 100) {
         userChoice = Number(prompt("Try again: "))
     }
 })
 
-
+buildBtn.addEventListener('click', () => {
+    
+})
 
